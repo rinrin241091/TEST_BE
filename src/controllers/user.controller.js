bb4901e80912e26043de9b82522d61984a2a77a1
nguyenminhtu17 <107929794+nguyenminhtu17@users.noreply.github.com>
2025-04-11 14:29:15 +0700

    update line control, services

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,9 @@
-
-const { validationResult } = require('express-validator');
-const userServices = require('../services/user.service');
+const { validationResult } = require("express-validator");
+const userServices = require("../services/user.service");
 
 const registerUser = async (req, res) => {
-  console.log('Processing registration request:', req.body);
-  
+  console.log("Processing registration request:", req.body);
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
@@ -13,25 +12,29 @@ const registerUser = async (req, res) => {
 
   try {
     const result = await userServices.register({ username, email, password });
-    res.status(201).json({ message: 'Đăng ký thành công', userId: result.insertId });
+    res
+      .status(201)
+      .json({ message: "Đăng ký thành công", userId: result.insertId });
   } catch (error) {
-    res.status(500).json({ message: 'Đã xảy ra lỗi khi đăng ký người dùng', error: error.message });
+    res.status(500).json({
+      message: "Đã xảy ra lỗi khi đăng ký người dùng",
+      error: error.message,
+    });
   }
 };
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    const result = await userServices.login({ username, password });
+    const result = await userServices.login({ email, password });
 
     // Trả về kết quả từ service, bao gồm token và thông tin người dùng
     res.status(200).json({
       accessToken: result.accessToken,
       refreshToken: result.refreshToken,
-      username: result.username,
+      email: result.email,
     });
-
   } catch (error) {
     console.error("Controller login error:", error.message);
     res.status(500).json({ message: error.message });
@@ -44,7 +47,7 @@ const forgotPassword = async (req, res) => {
     const message = await userServices.forgotPassword(email);
     res.status(200).json({ message });
   } catch (error) {
-    console.error('Error in forgotPassword controller:', error.message);
+    console.error("Error in forgotPassword controller:", error.message);
     res.status(500).json({ message: error.message });
   }
 };
@@ -53,10 +56,17 @@ const verifyOTPAndUpdatePassword = async (req, res) => {
   const { email, otp, newPassword } = req.body;
 
   try {
-    const message = await userServices.verifyOTPAndUpdatePassword(email, otp, newPassword);
+    const message = await userServices.verifyOTPAndUpdatePassword(
+      email,
+      otp,
+      newPassword
+    );
     res.status(200).json({ message });
   } catch (error) {
-    console.error('Error in verifyOTPAndUpdatePassword controller:', error.message);
+    console.error(
+      "Error in verifyOTPAndUpdatePassword controller:",
+      error.message
+    );
     res.status(500).json({ message: error.message });
   }
 };
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,15 +1,15 @@
 // models/userModel.js
-const db = require('../config/db');
-const bcrypt = require('bcryptjs');
-const nodemailer = require('nodemailer');
-const { sendEmail } = require('../utils/mail'); 
+const db = require("../config/db");
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const { sendEmail } = require("../utils/mail");
 const { verifyToken, generateToken } = require("../utils/jwt");
 
 const register = async (userData) => {
   try {
-
     const hashedPassword = await bcrypt.hash(userData.password, 10);
-    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+    const query =
+      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
     const values = [userData.username, userData.email, hashedPassword];
 
     return new Promise((resolve, reject) => {
@@ -22,34 +22,34 @@ const register = async (userData) => {
       });
     });
   } catch (error) {
-    throw new Error('Không thể tạo người dùng');
+    throw new Error("Không thể tạo người dùng");
   }
 };
 
 const login = async (data) => {
-  const { username, password } = data;
+  const { email, password } = data;
 
   try {
-    const queryUser = 'SELECT * FROM users WHERE username = ?';
-    const [user] = await db.promise().query(queryUser, [username]);
+    const queryUser = "SELECT * FROM users WHERE email = ?";
+    const [user] = await db.promise().query(queryUser, [email]);
 
     if (!user || user.length === 0) {
-      throw new Error('Tên người dùng hoặc mật khẩu không chính xác');
+      throw new Error("Tên người dùng hoặc mật khẩu không chính xác");
     }
 
     const foundUser = user[0]; // Lấy người dùng đầu tiên từ kết quả query
 
-      if (!password || !foundUser.password) {
-        throw new Error('Mật khẩu không hợp lệ');
-      }
+    if (!password || !foundUser.password) {
+      throw new Error("Mật khẩu không hợp lệ");
+    }
 
     const isPasswordMatch = await bcrypt.compare(password, foundUser.password);
     if (!isPasswordMatch) {
-      throw new Error('Tên người dùng hoặc mật khẩu không chính xác');
+      throw new Error("Tên người dùng hoặc mật khẩu không chính xác");
     }
 
     const payload = {
-      sub: foundUser.username,
+      sub: foundUser.email,
     };
 
     const { accessToken, refreshToken } = await generateToken(payload);
@@ -57,51 +57,51 @@ const login = async (data) => {
     return {
       accessToken,
       refreshToken,
-      username: foundUser.username,
+      email: foundUser.email,
     };
-
   } catch (error) {
     console.error("Login error:", error.message);
-    throw new Error('Đã xảy ra lỗi khi đăng nhập. Vui lòng thử lại sau.');
+    throw new Error("Đã xảy ra lỗi khi đăng nhập. Vui lòng thử lại sau.");
   }
 };
 
 const generateOTP = () => {
   const otpCode = Math.floor(100000 + Math.random() * 900000).toString();
-    return otpCode;
+  return otpCode;
 };
 
 const sendOTP = async (email, otp) => {
-  const subject = 'Mã OTP xác thực thay đổi mật khẩu';
+  const subject = "Mã OTP xác thực thay đổi mật khẩu";
   const text = `Mã OTP của bạn là: ${otp}. Mã này chỉ có hiệu lực trong 10 phút.`;
   const html = `<p>Mã OTP của bạn là: <strong>${otp}</strong></p><p>Mã này chỉ có hiệu lực trong 10 phút.</p>`;
 
   try {
-    await sendEmail(email, subject, text, html); 
-    console.log('OTP đã được gửi thành công');
+    await sendEmail(email, subject, text, html);
+    console.log("OTP đã được gửi thành công");
   } catch (error) {
-    console.error('Lỗi khi gửi OTP:', error);
-    throw new Error('Không thể gửi OTP. Vui lòng thử lại sau.');
+    console.error("Lỗi khi gửi OTP:", error);
+    throw new Error("Không thể gửi OTP. Vui lòng thử lại sau.");
   }
 };
 
 const forgotPassword = async (email) => {
   try {
-    const queryUser = 'SELECT * FROM users WHERE email = ?';
+    const queryUser = "SELECT * FROM users WHERE email = ?";
     const [user] = await db.promise().query(queryUser, [email]);
 
     if (!user || user.length === 0) {
-      throw new Error('Email không tồn tại trong hệ thống');
+      throw new Error("Email không tồn tại trong hệ thống");
     }
 
-    const otp = generateOTP(); 
-    await sendOTP(email, otp);   
+    const otp = generateOTP();
+    await sendOTP(email, otp);
 
     // Lưu OTP vào bảng OTP riêng (bảng otp)
-    const insertOTPQuery = 'INSERT INTO otp (email, otp, expires_at) VALUES (?, ?, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 10 MINUTE))';
+    const insertOTPQuery =
+      "INSERT INTO otp (email, otp, expires_at) VALUES (?, ?, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 10 MINUTE))";
     await db.promise().query(insertOTPQuery, [email, otp]);
 
-    return 'OTP đã được gửi đến email của bạn. Vui lòng kiểm tra email để tiếp tục.';
+    return "OTP đã được gửi đến email của bạn. Vui lòng kiểm tra email để tiếp tục.";
   } catch (error) {
     throw new Error(error.message);
   }
@@ -110,23 +110,24 @@ const forgotPassword = async (email) => {
 const verifyOTPAndUpdatePassword = async (email, otp, newPassword) => {
   try {
     // Kiểm tra OTP trong bảng otp
-    const queryOTP = 'SELECT * FROM otp WHERE email = ? AND otp = ? AND expires_at > CURRENT_TIMESTAMP';
+    const queryOTP =
+      "SELECT * FROM otp WHERE email = ? AND otp = ? AND expires_at > CURRENT_TIMESTAMP";
     const [otpRecord] = await db.promise().query(queryOTP, [email, otp]);
 
     if (!otpRecord || otpRecord.length === 0) {
-      throw new Error('Mã OTP không hợp lệ hoặc đã hết hạn');
+      throw new Error("Mã OTP không hợp lệ hoặc đã hết hạn");
     }
 
     // Mã OTP hợp lệ, tiếp tục cập nhật mật khẩu mới
-    const hashedPassword = await bcrypt.hash(newPassword, 10);  // Mã hóa mật khẩu mới
-    const updatePasswordQuery = 'UPDATE users SET password = ? WHERE email = ?';
+    const hashedPassword = await bcrypt.hash(newPassword, 10); // Mã hóa mật khẩu mới
+    const updatePasswordQuery = "UPDATE users SET password = ? WHERE email = ?";
     await db.promise().query(updatePasswordQuery, [hashedPassword, email]);
 
     // Xóa OTP sau khi sử dụng
-    const deleteOTPQuery = 'DELETE FROM otp WHERE email = ?';
+    const deleteOTPQuery = "DELETE FROM otp WHERE email = ?";
     await db.promise().query(deleteOTPQuery, [email]);
 
-    return 'Mật khẩu đã được thay đổi thành công.';
+    return "Mật khẩu đã được thay đổi thành công.";
   } catch (error) {
     throw new Error(error.message);
   }
